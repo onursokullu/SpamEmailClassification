a65c14d5fa8245cae7c0bd1c11b61af460479487
onursokullu <sokulluonur@hotmail.com>
2019-11-29 13:46:26 +0300

    naive bayes and knn implemented

--- SpamEmailDet.py
+++ SpamEmailDet.py
@@ -9,6 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score,confusion_matrix,f1_score
+from sklearn.neighbors import KNeighborsClassifier
 
 df = pd.read_csv("spam.csv")
 
@@ -23,4 +24,16 @@ xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2,train_size=0
 tfvec = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)#burada tfidvectorizer sınıfından bir nesne oluş
 xTrainFeat = tfvec.fit_transform(xTrain)#Bir üst satırda oluşturduğumuz nesneyi Training pandas serisine uydurduk pandas serileri tek boyutlu numpy dizilerine çok benzer
 xTestFeat =tfvec.transform(xTest)
+
+# yTrainToInt
+yTrainToInt = yTrain.astype('int')
+#Multinomial Naive bayes implementation
+classifiermodel = MultinomialNB()
+classifiermodel.fit(xTrainFeat,yTrainToInt)
+predResult = classifiermodel.predict(xTestFeat)
+
+#Knn implementation
+classifierModelKnn = KNeighborsClassifier()
+classifierModelKnn.fit(xTrainFeat,yTrainToInt)
+predResultKnn = classifierModelKnn.predict(xTestFeat)
 
